124ca0156bdf618ac15568a1c5aa3e4feac281a8
Riyadh <riyadh.ju@gmail.com>
2023-02-06 22:43:02 +0600

    added vercel

--- src/redux/thunk/post.js
+++ src/redux/thunk/post.js
@@ -1,24 +1,28 @@
 import axios from "axios";
 import { addContent, addTags } from "../actionCreators/actionCreators";
-export const postContent = (content) =>{
-    console.log(content)
-return async (dispatch , getState)=>{
-     await  axios.post("http://localhost:5000/content",content).then(res =>{
-        console.log(res.data)
-        if(res.data.acknowledged){
-          dispatch(addContent(content))
+export const postContent = (content) => {
+  console.log(content);
+  return async (dispatch, getState) => {
+    await axios
+      .post("https://linkdiary-server.vercel.app/content", content)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.acknowledged) {
+          dispatch(addContent(content));
         }
-      })
-}
-}
-export const postTags = (tags) =>{
-    console.log(tags)
-return async (dispatch , getState)=>{
-     await  axios.post("http://localhost:5000/tags",tags).then(res =>{
-        console.log(res.data)
-        if(res.data.acknowledged){
-          dispatch(addTags(tags))
+      });
+  };
+};
+export const postTags = (tags) => {
+  console.log(tags);
+  return async (dispatch, getState) => {
+    await axios
+      .post("https://linkdiary-server.vercel.app/tags", tags)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.acknowledged) {
+          dispatch(addTags(tags));
         }
-      })
-}
-}
+      });
+  };
+};
